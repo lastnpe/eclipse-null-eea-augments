47da09605a537d691a4d4948e1028a72b5f091e4
Michael Vorburger <mike@vorburger.ch>
2017-10-03 16:46:19 +0200

    @NonNullByDefault with TYPE_PARAMETER & ARRAY_CONTENTS in package-info

--- examples/maven/hello/src/main/java/org/lastnpe/examples/hello/GenericsExample.java
+++ examples/maven/hello/src/main/java/org/lastnpe/examples/hello/GenericsExample.java
@@ -1,17 +1,6 @@
 package org.lastnpe.examples.hello;
 
-import static org.eclipse.jdt.annotation.DefaultLocation.ARRAY_CONTENTS;
-import static org.eclipse.jdt.annotation.DefaultLocation.FIELD;
-import static org.eclipse.jdt.annotation.DefaultLocation.PARAMETER;
-import static org.eclipse.jdt.annotation.DefaultLocation.RETURN_TYPE;
-import static org.eclipse.jdt.annotation.DefaultLocation.TYPE_ARGUMENT;
-import static org.eclipse.jdt.annotation.DefaultLocation.TYPE_BOUND;
-import static org.eclipse.jdt.annotation.DefaultLocation.TYPE_PARAMETER;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
 @SuppressWarnings("unused")
-@NonNullByDefault({ PARAMETER, RETURN_TYPE, FIELD, TYPE_PARAMETER, TYPE_BOUND, TYPE_ARGUMENT, ARRAY_CONTENTS })
 public class GenericsExample {
 
     private static class Parent<T> {
