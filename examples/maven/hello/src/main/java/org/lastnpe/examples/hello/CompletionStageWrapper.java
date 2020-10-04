0a71dfef16ab41084e5d1e1997c841e4d26dc897
Jan N. Klug <jan.n.klug@rub.de>
2020-10-04 14:11:42 +0200

    fix build
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- examples/maven/hello/src/main/java/org/lastnpe/examples/hello/CompletionStageWrapper.java
+++ examples/maven/hello/src/main/java/org/lastnpe/examples/hello/CompletionStageWrapper.java
@@ -1,20 +1,10 @@
 package org.lastnpe.examples.hello;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
-import static org.eclipse.jdt.annotation.DefaultLocation.ARRAY_CONTENTS;
-import static org.eclipse.jdt.annotation.DefaultLocation.FIELD;
-import static org.eclipse.jdt.annotation.DefaultLocation.PARAMETER;
-import static org.eclipse.jdt.annotation.DefaultLocation.RETURN_TYPE;
-import static org.eclipse.jdt.annotation.DefaultLocation.TYPE_ARGUMENT;
-import static org.eclipse.jdt.annotation.DefaultLocation.TYPE_BOUND;
-import static org.eclipse.jdt.annotation.DefaultLocation.TYPE_PARAMETER;
-
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Executor;
 import java.util.function.Function;
 
-@NonNullByDefault({ PARAMETER, RETURN_TYPE, FIELD, TYPE_PARAMETER, TYPE_BOUND, TYPE_ARGUMENT, ARRAY_CONTENTS })
 public abstract class CompletionStageWrapper<T> implements CompletionStage<T> {
 
     @Override
