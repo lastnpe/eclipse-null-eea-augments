5b59e61cca73600872d625e8578dce90c977d46a
sebthom <sebthom@users.noreply.github.com>
2024-09-30 21:28:36 +0200

    refact: reactivate examples
    Signed-off-by: sebthom <sebthom@users.noreply.github.com>

--- examples/maven/hello/src/main/java/org/lastnpe/examples/hello/GenericsExample.java
+++ examples/maven/hello/src/main/java/org/lastnpe/examples/hello/GenericsExample.java
@@ -1,17 +1,11 @@
 package org.lastnpe.examples.hello;
 
-import org.eclipse.jdt.annotation.NonNull;
-
 @SuppressWarnings("unused")
 public class GenericsExample {
 
     private static class Parent<T> {
     }
 
-    // Null constraint mismatch: The type 'T' is not a valid substitute for the type parameter '@NonNull T'
-    // This seems wrong, because the mouse over hover clearly shows Parent<@NonNull T> (from @NonNullByDefault) and Child<@NonNull T>
-    // Workaround "Child<@NonNull T>" makes it disappear, but why is this needed?
-    // TODO Is this https://bugs.eclipse.org/bugs/show_bug.cgi?id=522142 ?
-    private static class Child<@NonNull T> extends Parent<T> {
+    private static class Child<T> extends Parent<T> {
     }
 }
